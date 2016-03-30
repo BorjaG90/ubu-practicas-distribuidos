de81a7949df872537a1f41f3ed676328b790de92
Plamen Petkov <petkov092@gmail.com>
2016-03-30 17:05:23 +0200

    Clase ChatServerImpl.java parcialmente implementada. Falta implementar detalles, metodo shutdown y metodo run.

--- Chat 1.0/src/es/ubu/lsi/server/ChatServerImpl.java
+++ Chat 1.0/src/es/ubu/lsi/server/ChatServerImpl.java
@@ -1,62 +1,132 @@
 package es.ubu.lsi.server;
-/**Clase ChatServerImpl
- * Implementacion del servidor del chat
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import java.text.SimpleDateFormat;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import es.ubu.lsi.common.ChatMessage;
+
+/**
+ * Clase ChatServerImpl Implementacion del servidor del chat
+ * 
  * @author Borja Gete & Plamen Petkov
  * @version 1.0.0
  */
-import java.text.SimpleDateFormat;
-import es.ubu.lsi.common.ChatMessage;
+public class ChatServerImpl implements ChatServer {
 
-public class ChatServerImpl implements ChatServer{
-	
-	private final int DEFAULT_PORT = 1500;
-	private int clientId;
-	private SimpleDateFormat sdf;
+	private static final int DEFAULT_PORT = 1500;
+	private static int clientId;
+	private static SimpleDateFormat sdf;
 	private int port;
 	private boolean alive;
-	
-	public ChatServerImpl(int port) {
-		
+
+	ServerSocket serverSocket;
+	Map<Integer, ChatServerThreadForClient> clients;
+
+	public ChatServerImpl() throws IOException {
+		this(DEFAULT_PORT);
 	}
-	
+
+	public ChatServerImpl(int port) throws IOException {
+		alive = true;
+		this.port = port;
+		serverSocket = new ServerSocket(port);
+		clients = new HashMap<Integer, ChatServerThreadForClient>();
+	}
+
 	@Override
 	public void startup() {
-		// TODO Auto-generated method stub
-		
+
+		int id;
+		String username;
+		ChatMessage request;
+		Socket clientSocket;
+		ChatServerThreadForClient clientThread;
+
+		try {
+			while (alive) {
+
+				clientSocket = serverSocket.accept();
+
+				request = (ChatMessage) new ObjectInputStream(clientSocket.getInputStream()).readObject();
+
+				id = request.getId();
+				username = request.getMessage();
+
+				if (clients.get(id) == null) {
+					clientThread = new ChatServerThreadForClient(id, username, clientSocket);
+					clients.put(id, clientThread);
+					clientThread.start();
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
 	public void shutdown() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
+	// es probable que tenga que ser un metodo synchronized
 	public void broadcast(ChatMessage message) {
-		// TODO Auto-generated method stub
-		
+		for (int id : clients.keySet())
+			if (id != clientId)
+				try {
+					clients.get(id).out.writeObject(message);
+				} catch (IOException e) {
+					System.err.println("Could not send message to user " + clients.get(id).username);
+				}
 	}
 
 	@Override
 	public void remove(int id) {
-		// TODO Auto-generated method stub
-		
+		clients.remove(id);
 	}
+
 	class ChatServerThreadForClient extends Thread {
 
 		private int id;
 		private String username;
-		
-		public ChatServerThreadForClient(int id, String username) {
+		private Socket socket;
+
+		private ObjectInputStream in;
+		private ObjectOutputStream out;
+
+		public ChatServerThreadForClient(int id, String username, Socket socket) throws IOException {
 			super(username);
 			this.id = id;
+			this.socket = socket;
+			in = new ObjectInputStream(socket.getInputStream());
+			out = new ObjectOutputStream(socket.getOutputStream());
 		}
+
+		@Override
 		public void run() {
-			
+
 		}
 	}
-	public static void main(String[] args){
-		
+
+	public static void main(String[] args) throws IOException {
+
+		ChatServerImpl server = new ChatServerImpl();
+		server.startup();
 	}
 
 }
